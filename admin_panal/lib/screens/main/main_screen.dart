8f2a3974f5b6c657732f4fd599357a856bd5bf6a
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-05-09 11:50:22 +0530

    Refactor BrandHeader widget text
    The `BrandHeader` widget's text was changed from "Brands" to "Brands ".

--- admin_panal/lib/screens/main/main_screen.dart
+++ admin_panal/lib/screens/main/main_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'components/side_menu.dart';
 
-
-
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
