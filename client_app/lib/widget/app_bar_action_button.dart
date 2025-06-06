74533ae543be2d2982b2215f4607b1298cde857b
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-06 23:29:11 +0530

    update

--- client_app/lib/widget/app_bar_action_button.dart
+++ client_app/lib/widget/app_bar_action_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../../utility/app_color.dart';
 
-
 class AppBarActionButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
