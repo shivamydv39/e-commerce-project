0b8dc965225d747d019d2851d6a8f85f157d25e1
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-07 18:30:34 +0530

    update

--- client_app/lib/main.dart
+++ client_app/lib/main.dart
@@ -18,7 +18,6 @@ import 'package:provider/provider.dart';
 import 'core/data/data_provider.dart';
 import 'models/user.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
@@ -31,18 +30,24 @@ Future<void> main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => DataProvider()),
-        ChangeNotifierProvider(create: (context) => UserProvider(context.dataProvider)),
-        ChangeNotifierProvider(create: (context) => ProfileProvider(context.dataProvider)),
-        ChangeNotifierProvider(create: (context) => ProductByCategoryProvider(context.dataProvider)),
-        ChangeNotifierProvider(create: (context) => ProductDetailProvider(context.dataProvider)),
-        ChangeNotifierProvider(create: (context) => CartProvider(context.userProvider)),
-        ChangeNotifierProvider(create: (context) => FavoriteProvider(context.dataProvider)),
+        ChangeNotifierProvider(
+            create: (context) => UserProvider(context.dataProvider)),
+        ChangeNotifierProvider(
+            create: (context) => ProfileProvider(context.dataProvider)),
+        ChangeNotifierProvider(
+            create: (context) =>
+                ProductByCategoryProvider(context.dataProvider)),
+        ChangeNotifierProvider(
+            create: (context) => ProductDetailProvider(context.dataProvider)),
+        ChangeNotifierProvider(
+            create: (context) => CartProvider(context.userProvider)),
+        ChangeNotifierProvider(
+            create: (context) => FavoriteProvider(context.dataProvider)),
       ],
       child: const MyApp(),
     ),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
