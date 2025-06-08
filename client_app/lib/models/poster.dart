f40f4676f790550395d135494944a402083118ff
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-08 17:25:33 +0530

    update

--- client_app/lib/models/poster.dart
+++ client_app/lib/models/poster.dart
@@ -8,11 +8,11 @@ class Poster {
 
   Poster(
       {this.sId,
-        this.posterName,
-        this.imageUrl,
-        this.createdAt,
-        this.updatedAt,
-        this.iV});
+      this.posterName,
+      this.imageUrl,
+      this.createdAt,
+      this.updatedAt,
+      this.iV});
 
   Poster.fromJson(Map<String, dynamic> json) {
     sId = json['_id'];
