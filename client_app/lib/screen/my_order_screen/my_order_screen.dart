505e06f1ef30c83ecc5857eadd46999bd498d3c7
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-09 18:07:11 +0530

    update

--- client_app/lib/screen/my_order_screen/my_order_screen.dart
+++ client_app/lib/screen/my_order_screen/my_order_screen.dart
@@ -8,7 +8,6 @@ import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import '../../widget/order_tile.dart';
 
-
 class MyOrderScreen extends StatelessWidget {
   const MyOrderScreen({super.key});
 
@@ -18,7 +17,10 @@ class MyOrderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           "My Orders",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: AppColor.darkOrange),
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: AppColor.darkOrange),
         ),
       ),
       body: Consumer<DataProvider>(
@@ -29,11 +31,12 @@ class MyOrderScreen extends StatelessWidget {
               final order = context.dataProvider.orders[index];
               return OrderTile(
                 paymentMethod: order.paymentMethod ?? '',
-                items: '${(order.items.safeElementAt(0)?.productName ?? '')} & ${order.items!.length - 1} Items'  ,
+                items:
+                    '${(order.items.safeElementAt(0)?.productName ?? '')} & ${order.items!.length - 1} Items',
                 date: order.orderDate ?? '',
                 status: order.orderStatus ?? 'pending',
-                onTap: (){
-                  if(order.orderStatus == 'shipped'){
+                onTap: () {
+                  if (order.orderStatus == 'shipped') {
                     Get.to(TrackingScreen(url: order.trackingUrl ?? ''));
                   }
                 },
