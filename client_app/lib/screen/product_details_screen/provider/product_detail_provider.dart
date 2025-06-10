47772d8716f87e414082cd72be2eb7610e852c74
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-10 15:14:49 +0530

    update

--- client_app/lib/screen/product_details_screen/provider/product_detail_provider.dart
+++ client_app/lib/screen/product_details_screen/provider/product_detail_provider.dart
@@ -5,7 +5,6 @@ import '../../../core/data/data_provider.dart';
 import '../../../models/product.dart';
 import '../../../utility/snack_bar_helper.dart';
 
-
 class ProductDetailProvider extends ChangeNotifier {
   final DataProvider _dataProvider;
   String? selectedVariant;
@@ -14,18 +13,20 @@ class ProductDetailProvider extends ChangeNotifier {
   ProductDetailProvider(this._dataProvider);
 
   void addToCart(Product product) {
-    if(product.proVariantId!.isNotEmpty && selectedVariant == null){
+    if (product.proVariantId!.isNotEmpty && selectedVariant == null) {
       SnackBarHelper.showErrorSnackBar('Pleas select a variant');
       return;
     }
-    double? price = product.offerPrice != product.price ? product.offerPrice : product.price;
+    double? price = product.offerPrice != product.price
+        ? product.offerPrice
+        : product.price;
     flutterCart.addToCart(
       cartModel: CartModel(
           productId: '${product.sId}',
           productName: '${product.name}',
           productImages: ['${product.images.safeElementAt(0)?.url}'],
           variants: [ProductVariant(price: price ?? 0, color: selectedVariant)],
-          productDetails: '${product.description}'),  // CartModel
+          productDetails: '${product.description}'), // CartModel
     );
     selectedVariant = null;
     SnackBarHelper.showSuccessSnackBar('Item Added');
