18b0d2f73080c6b294a2d9e98e4dc680a04fd91c
Shivamyadav48 <shivam.2022ug1048@iiitranchi.ac.in>
2025-06-22 11:15:43 +0530

    update

--- client_app/lib/utility/app_theme.dart
+++ client_app/lib/utility/app_theme.dart
@@ -22,6 +22,7 @@ class AppTheme {
         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
       ),
     ),
+
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(foregroundColor: Colors.deepOrange),
     ),
